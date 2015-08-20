d71970695a52c63850ed30ada7f670384f10e391
Bartek . <qrdupel1996@gmail.com>
2015-08-20 12:48:51 +0200

    Update message.js
    completed createDOM() and getter

--- js/message.js
+++ js/message.js
@@ -2,16 +2,33 @@ function Message(data){
 	this.id = Number(data.postid);
 
 	this.user = {
-		id: Number(data.userid),
-		name: data.username
+		id: Number(data.userid)
+		,name: data.username
 	};
 
 	this.value = data.message;
-	this.posted = new Date(data.posted);
+	this.posted = data.posted
 
 	this.$el = null; //after createDOM() -> div.message
 }
 
 Message.prototype.createDOM = function(){
 	this.$el = document.createElement('div');
+	this.$el.setAttribute('id', 'min-message' + this.id);
+	this.$el.setAttribute('title', 'Wysłano: ' + this.posted);
+	this.$el.classList.add('min-message');
+
+
+	var $a = this.$el.createElement('a');
+		$a.setAttribute('href', this.user.name.split(' ').join('+')); //escape() is strange O_o
+		$a.innerText = this.user.name;
+
+
+	var $c = this.$el.createElement('span');
+		$c.innerHTML = this.value;  //value is already escaped from html tags
+}
+
+Message.prototype.getDOM = function(){
+	//too lame
+	return this.$el;
 }
