06233743e7ca0198b3036a669464cc691694010a
Krzysztof Pazdur <pazdurk@gmail.com>
2015-09-27 18:33:03 +0200

    Code Rearrange

--- js/skeleton.js
+++ js/skeleton.js
@@ -1,15 +1,15 @@
 document.body.innerHTML +=
-'<section class="min-chat"> \
-    <header class="min-chat__header"> \
-        <b class="min-chat__header__title">Czatrepajr</b> \
-     <button id="min-chat-switch" class="min-chat__header__switch">Wyłącz</button> \
-    </header> \
-    <div id="min-chat-content"> \
-        <div class="min-chat__notify min-chat__notify--left min-chat__notify--hidden"></div> \
-        <div class="min-chat__notify min-chat__notify--join min-chat__notify--hidden"></div> \
-        <div class="min-chat__content"></div> \
-        <div class="min-chat__fieldset"> \
-            <input type="text" id="chat-input" class="min-chat__fieldset__input" spellcheck="true"> \
-        </div> \
-  </div> \
-</section>';
+    '<section class="min-chat"> \
+        <header class="min-chat__header"> \
+            <b class="min-chat__header__title">Czatrepajr</b> \
+         <button id="min-chat-switch" class="min-chat__header__switch">Wyłącz</button> \
+        </header> \
+        <div id="min-chat-content"> \
+            <div class="min-chat__notify min-chat__notify--left min-chat__notify--hidden"></div> \
+            <div class="min-chat__notify min-chat__notify--join min-chat__notify--hidden"></div> \
+            <div class="min-chat__content"></div> \
+            <div class="min-chat__fieldset"> \
+                <input type="text" id="chat-input" class="min-chat__fieldset__input" spellcheck="true"> \
+            </div> \
+      </div> \
+    </section>';
