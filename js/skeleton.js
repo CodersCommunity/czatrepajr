9fa2e21753e2d85a95d3df75721f99ed139c5e57
magic96 <qrdupel1996@gmail.com>
2015-12-31 00:58:13 +0100

    Rewritten skeleton from string template to HTMLElements, tiny changes in classes naming.

--- js/skeleton.js
+++ js/skeleton.js
@@ -1,16 +1,79 @@
-$(`<section class="min-chat">
-<header class="min-chat__header">
-    <b class="min-chat__header__title">
-            <img src="http://i.imgur.com/RKJMsl8.png" class="min-chat__icon" >
-            Czatrepajr</b>
- <button id="min-chat-switch" class="min-chat__header__switch" title="Wyłącz">↓</button> 
-</header>
-<div id="min-chat-content">
-    <div class="min-chat__notify min-chat__notify--left min-chat__notify--hidden"></div>
-    <div class="min-chat__notify min-chat__notify--join min-chat__notify--hidden"></div>
-    <div class="min-chat__content"></div>
-    <div class="min-chat__fieldset">
-        <input type="text" id="chat-input" class="min-chat__fieldset__input" spellcheck="true">
-    </div>
-</div>
-</section>`).appendTo('body');
+(function() {
+	"use strict";
+
+	var prefix = "min-chat"
+		, section
+		, header, headerTitle, headerSpan, headerIcon, headerSwitch
+		, wrapper, notifyLeft, notifyJoin, content, fieldSet, input
+	;
+
+	section = document.createElement( "section" );
+	section.classList.add( prefix );
+
+	header = document.createElement( "header" );
+	header.classList.add( prefix + "__header" );
+
+	headerTitle = document.createElement( "b" );
+	headerTitle.classList.add( prefix + "__header__title" );
+
+	headerSpan = document.createElement( "span" );
+	headerSpan.innerHTML = "CzatRepajr";
+
+	headerIcon = document.createElement( "img" );
+	headerIcon.classList.add( prefix + "__header__icon" );
+	headerIcon.src = "http://i.imgur.com/RKJMsl8.png";
+
+	headerSwitch = document.createElement( "button" );
+	headerSwitch.id = prefix + "-switch";
+	headerSwitch.title = "Wyłącz";
+	headerSwitch.classList.add( prefix + "__header__switch" );
+	headerSwitch.innerHTML = "↓";
+
+	wrapper = document.createElement( "div" );
+	wrapper.id = prefix + "-content";
+
+	notifyLeft = document.createElement( "div" );
+	notifyLeft.classList.add( prefix + "__notify", prefix + "__notify--left", prefix + "__notify--hidden" );
+
+	notifyJoin = document.createElement("div");
+	notifyJoin.classList.add( prefix + "__notify", prefix + "__notify--left", prefix + "__notify--hidden" );
+
+	content = document.createElement( "div" );
+	content.classList.add( prefix + "__content" );
+
+	fieldSet = document.createElement( "div" );
+	fieldSet.classList.add( prefix + "__fieldset" );
+
+	input = document.createElement( "input" );
+	input.type = "text";
+	input.id = "chat-input"; //whether should be min-chat-input?
+	input.spellcheck = true;
+	input.classList.add( prefix + "__fieldset__input" );
+
+
+	/* WRAPPER ELEMENTS */
+
+	fieldSet.appendChild( input );
+
+	wrapper.appendChild( notifyLeft );
+	wrapper.appendChild( notifyJoin );
+	wrapper.appendChild( content );
+	wrapper.appendChild( fieldSet );
+
+	/* HEADER ELEMENTS */
+
+	headerTitle.appendChild( headerIcon ); // i think it is not correctly
+	headerTitle.appendChild( headerSpan ); // if element above has been improved it should be removed
+
+	header.appendChild( headerTitle );
+	header.appendChild( headerSwitch );
+
+	/* SECTION ELEMENTS */
+
+	section.appendChild( header );
+	section.appendChild( wrapper );
+
+
+	/* FINALLY */
+	document.body.appendChild( section );
+})();
