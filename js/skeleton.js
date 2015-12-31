78cab466ce00c0b3b7799aa7a60d2e247707857e
magic96 <qrdupel1996@gmail.com>
2015-12-31 01:16:14 +0100

    Replaced tabulations to spaces(2).

--- js/skeleton.js
+++ js/skeleton.js
@@ -1,79 +1,79 @@
 (function() {
-	"use strict";
+  "use strict";
 
-	var prefix = "min-chat"
-		, section
-		, header, headerTitle, headerSpan, headerIcon, headerSwitch
-		, wrapper, notifyLeft, notifyJoin, content, fieldSet, input
-	;
+  var prefix = "min-chat"
+    , section
+    , header, headerTitle, headerSpan, headerIcon, headerSwitch
+    , wrapper, notifyLeft, notifyJoin, content, fieldSet, input
+  ;
 
-	section = document.createElement( "section" );
-	section.classList.add( prefix );
+  section = document.createElement( "section" );
+  section.classList.add( prefix );
 
-	header = document.createElement( "header" );
-	header.classList.add( prefix + "__header" );
+  header = document.createElement( "header" );
+  header.classList.add( prefix + "__header" );
 
-	headerTitle = document.createElement( "b" );
-	headerTitle.classList.add( prefix + "__header__title" );
+  headerTitle = document.createElement( "b" );
+  headerTitle.classList.add( prefix + "__header__title" );
 
-	headerSpan = document.createElement( "span" );
-	headerSpan.innerHTML = "CzatRepajr";
+  headerSpan = document.createElement( "span" );
+  headerSpan.innerHTML = "CzatRepajr";
 
-	headerIcon = document.createElement( "img" );
-	headerIcon.classList.add( prefix + "__header__icon" );
-	headerIcon.src = "http://i.imgur.com/RKJMsl8.png";
+  headerIcon = document.createElement( "img" );
+  headerIcon.classList.add( prefix + "__header__icon" );
+  headerIcon.src = "http://i.imgur.com/RKJMsl8.png";
 
-	headerSwitch = document.createElement( "button" );
-	headerSwitch.id = prefix + "-switch";
-	headerSwitch.title = "Wyłącz";
-	headerSwitch.classList.add( prefix + "__header__switch" );
-	headerSwitch.innerHTML = "↓";
+  headerSwitch = document.createElement( "button" );
+  headerSwitch.id = prefix + "-switch";
+  headerSwitch.title = "Wyłącz";
+  headerSwitch.classList.add( prefix + "__header__switch" );
+  headerSwitch.innerHTML = "↓";
 
-	wrapper = document.createElement( "div" );
-	wrapper.id = prefix + "-content";
+  wrapper = document.createElement( "div" );
+  wrapper.id = prefix + "-content";
 
-	notifyLeft = document.createElement( "div" );
-	notifyLeft.classList.add( prefix + "__notify", prefix + "__notify--left", prefix + "__notify--hidden" );
+  notifyLeft = document.createElement( "div" );
+  notifyLeft.classList.add( prefix + "__notify", prefix + "__notify--left", prefix + "__notify--hidden" );
 
-	notifyJoin = document.createElement("div");
-	notifyJoin.classList.add( prefix + "__notify", prefix + "__notify--left", prefix + "__notify--hidden" );
+  notifyJoin = document.createElement("div");
+  notifyJoin.classList.add( prefix + "__notify", prefix + "__notify--left", prefix + "__notify--hidden" );
 
-	content = document.createElement( "div" );
-	content.classList.add( prefix + "__content" );
+  content = document.createElement( "div" );
+  content.classList.add( prefix + "__content" );
 
-	fieldSet = document.createElement( "div" );
-	fieldSet.classList.add( prefix + "__fieldset" );
+  fieldSet = document.createElement( "div" );
+  fieldSet.classList.add( prefix + "__fieldset" );
 
-	input = document.createElement( "input" );
-	input.type = "text";
-	input.id = "chat-input"; //whether should be min-chat-input?
-	input.spellcheck = true;
-	input.classList.add( prefix + "__fieldset__input" );
-
-
-	/* WRAPPER ELEMENTS */
-
-	fieldSet.appendChild( input );
-
-	wrapper.appendChild( notifyLeft );
-	wrapper.appendChild( notifyJoin );
-	wrapper.appendChild( content );
-	wrapper.appendChild( fieldSet );
-
-	/* HEADER ELEMENTS */
-
-	headerTitle.appendChild( headerIcon ); // i think it is not correctly
-	headerTitle.appendChild( headerSpan ); // if element above has been improved it should be removed
-
-	header.appendChild( headerTitle );
-	header.appendChild( headerSwitch );
-
-	/* SECTION ELEMENTS */
-
-	section.appendChild( header );
-	section.appendChild( wrapper );
+  input = document.createElement( "input" );
+  input.type = "text";
+  input.id = "chat-input"; //whether should be min-chat-input?
+  input.spellcheck = true;
+  input.classList.add( prefix + "__fieldset__input" );
 
 
-	/* FINALLY */
-	document.body.appendChild( section );
+  /* WRAPPER ELEMENTS */
+
+  fieldSet.appendChild( input );
+
+  wrapper.appendChild( notifyLeft );
+  wrapper.appendChild( notifyJoin );
+  wrapper.appendChild( content );
+  wrapper.appendChild( fieldSet );
+
+  /* HEADER ELEMENTS */
+
+  headerTitle.appendChild( headerIcon ); // i think it is not correctly
+  headerTitle.appendChild( headerSpan ); // if element above has been improved it should be removed
+
+  header.appendChild( headerTitle );
+  header.appendChild( headerSwitch );
+
+  /* SECTION ELEMENTS */
+
+  section.appendChild( header );
+  section.appendChild( wrapper );
+
+
+  /* FINALLY */
+  document.body.appendChild( section );
 })();
